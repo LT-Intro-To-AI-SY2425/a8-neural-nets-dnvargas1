fa997288dae72be62df937d195f16a40213a6d93
Daniel Vargas <dnvargas1@cps.edu>
2025-04-25 10:38:16 -0500

    worked on it

--- a8.py
+++ a8.py
@@ -1,7 +1,11 @@
 from neural import *
 
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
+xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
 
+xortest = NeuralNet(2, 2, 1)
+xortest.train(xor_training_data)
+print(xortest.test_with_expected(xor_training_data))
 
 
 
